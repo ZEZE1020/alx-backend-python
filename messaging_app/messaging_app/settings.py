172aa89663c30bc29aa435e34d512b44cc0e7788
Godfrey <ogembogodfrey75@gmail.com>
2025-08-27 23:29:12 +0300

    feat(settings): Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'rest_framework',
 ]
 
 REST_FRAMEWORK = {
